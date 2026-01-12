eb4a8c02321c23f56e80966206e8cbf123845a3a
shreya-saran <118761328+shreya-saran@users.noreply.github.com>
2026-01-12 22:45:07 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -294,7 +294,7 @@ export default function ContactSection() {
                   24/7 Support
                 </div>
                 <a href="tel:+917838500048" className="text-sm" style={{ color: "#b85947" }}>
-                  +91 - 7838500048
+                  +91 - 9811333148
                 </a>
               </div>
             </div>
